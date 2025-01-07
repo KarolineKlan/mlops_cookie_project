0c927882b4e713aa5a4cb0ea2ef62ba173db7bf3
KarolineKlan <karolineklanhansen@gmail.com>
2025-01-07 14:10:19 +0100

    M6 Done and it runs and works

---
+++ main.py
@@ -0,0 +1,21 @@
+import mlops_cookie_project1.data as data 
+import mlops_cookie_project1.evaluate as evaluate
+import mlops_cookie_project1.train as train
+import mlops_cookie_project1.visualize as visualize
+
+def main():
+    # Run them in the correct order
+    data.preprocess_data("data/raw", "data/processed")
+    print("Data preprocessed")
+
+    train.train()
+    print("Model trained")
+
+    evaluate.evaluate("models/model.pth")
+    print("Model evaluated")
+
+    visualize.visualize("models/model.pth")
+    print("Model visualized")
+
+if __name__ == "__main__":
+    main()
