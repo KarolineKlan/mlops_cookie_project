e1f4b3327c5c890c6e620386e7b641eed9c9de18
KarolineKlan <karolineklanhansen@gmail.com>
2025-01-14 13:17:23 +0100

    after wandb before dvc exercise - hope i didnt fuckup

--- dockerfiles/train.dockerfile
+++ dockerfiles/train.dockerfile
@@ -1,17 +1,50 @@
-# Base image
-FROM python:3.11-slim AS base
 
+# Base image
+FROM python:3.11-slim
+
+# Install Python
 RUN apt update && \
     apt install --no-install-recommends -y build-essential gcc && \
     apt clean && rm -rf /var/lib/apt/lists/*
 
-COPY src src/
 COPY requirements.txt requirements.txt
-COPY requirements_dev.txt requirements_dev.txt
-COPY README.md README.md
 COPY pyproject.toml pyproject.toml
+COPY src/ src/
+COPY data/ data/
+COPY models/ models/
+COPY reports/ reports/
 
-RUN pip install -r requirements.txt --no-cache-dir --verbose
-RUN pip install . --no-deps --no-cache-dir --verbose
+
+WORKDIR /
+RUN pip install -r requirements.txt --no-cache-dir
+RUN pip install . --no-deps --no-cache-dir
 
 ENTRYPOINT ["python", "-u", "src/mlops_cookie_project1/train.py"]
+
+
+# # Base image
+# FROM python:3.11-slim AS base
+
+# RUN apt update && \
+#     apt install --no-install-recommends -y build-essential gcc && \
+#     apt clean && rm -rf /var/lib/apt/lists/*
+
+
+# COPY src src/
+# COPY requirements.txt requirements.txt
+# COPY requirements_dev.txt requirements_dev.txt
+# COPY README.md README.md
+# COPY pyproject.toml pyproject.toml
+
+
+# COPY mlops_cookie_project1/ mlops_cookie_project1/
+# COPY data/ data/
+
+# WORKDIR /
+# RUN pip install -r requirements.txt --no-cache-dir
+# RUN pip install . --no-deps --no-cache-dir
+
+# RUN pip install -r requirements.txt --no-cache-dir --verbose
+# RUN pip install . --no-deps --no-cache-dir --verbose
+
+# ENTRYPOINT ["python", "-u", "src/mlops_cookie_project1/train.py"]
--- src/mlops_cookie_project1/train.py
+++ src/mlops_cookie_project1/train.py
@@ -1,16 +1,21 @@
 import matplotlib.pyplot as plt
 import torch
 import typer
+import wandb
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, RocCurveDisplay
 from mlops_cookie_project1.data import corrupt_mnist
 from mlops_cookie_project1.model import MyAwesomeModel
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
-
-def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
+def train(lr: float = 0.001, batch_size: int = 32, epochs: int = 5) -> None:
     """Train a model on MNIST."""
     print("Training day and night")
     print(f"{lr=}, {batch_size=}, {epochs=}")
+    run = wandb.init(
+        project="corrupt_mnist",
+        config={"lr": lr, "batch_size": batch_size, "epochs": epochs},
+    )
 
     model = MyAwesomeModel().to(DEVICE)
     train_set, _ = corrupt_mnist()
@@ -20,9 +25,10 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-    statistics = {"train_loss": [], "train_accuracy": []}
     for epoch in range(epochs):
         model.train()
+
+        preds, targets = [], []
         for i, (img, target) in enumerate(train_dataloader):
             img, target = img.to(DEVICE), target.to(DEVICE)
             optimizer.zero_grad()
@@ -30,22 +36,55 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
             loss = loss_fn(y_pred, target)
             loss.backward()
             optimizer.step()
-            statistics["train_loss"].append(loss.item())
-
             accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
-            statistics["train_accuracy"].append(accuracy)
+            wandb.log({"train_loss": loss.item(), "train_accuracy": accuracy})
+
+            preds.append(y_pred.detach().cpu())
+            targets.append(target.detach().cpu())
 
             if i % 100 == 0:
                 print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
 
-    print("Training complete")
-    torch.save(model.state_dict(), "models/model.pth")
-    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
-    axs[0].plot(statistics["train_loss"])
-    axs[0].set_title("Train loss")
-    axs[1].plot(statistics["train_accuracy"])
-    axs[1].set_title("Train accuracy")
-    fig.savefig("reports/figures/training_statistics.png")
+                # add a plot of the input images
+                images = wandb.Image(img[:5].detach().cpu(), caption="Input images")
+                wandb.log({"images": images})
+
+                # add a plot of histogram of the gradients
+                grads = torch.cat([p.grad.flatten() for p in model.parameters() if p.grad is not None], 0)
+                wandb.log({"gradients": wandb.Histogram(grads.cpu())})
+
+        # add a custom matplotlib plot of the ROC curves
+        preds = torch.cat(preds, 0)
+        targets = torch.cat(targets, 0)
+
+        for class_id in range(10):
+            one_hot = torch.zeros_like(targets)
+            one_hot[targets == class_id] = 1
+            _ = RocCurveDisplay.from_predictions(
+                one_hot,
+                preds[:, class_id],
+                name=f"ROC curve for {class_id}",
+                plot_chance_level=(class_id == 2),
+            )
+
+        wandb.log({"roc": wandb.plot.roc_curve(targets, preds)})
+        # alternatively the wandb.plot.roc_curve function can be used
+
+    final_accuracy = accuracy_score(targets, preds.argmax(dim=1))
+    final_precision = precision_score(targets, preds.argmax(dim=1), average="weighted")
+    final_recall = recall_score(targets, preds.argmax(dim=1), average="weighted")
+    final_f1 = f1_score(targets, preds.argmax(dim=1), average="weighted")
+
+    # first we save the model to a file then log it as an artifact
+    torch.save(model.state_dict(), "model.pth")
+    artifact = wandb.Artifact(
+        name="corrupt_mnist_model",
+        type="model",
+        description="A model trained to classify corrupt MNIST images",
+        metadata={"accuracy": final_accuracy, "precision": final_precision, "recall": final_recall, "f1": final_f1},
+    )
+    artifact.add_file("model.pth")
+    run.log_artifact(artifact)
 
 
 if __name__ == "__main__":
